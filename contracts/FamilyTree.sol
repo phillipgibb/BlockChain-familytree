8d9a6620faac88a0a674cc226acbc4506394ef49
phillipgibb <phillipgibb@gmail.com>
2017-11-14 05:31:49 +0200

    Add add functions

--- contracts/FamilyTree.sol
+++ contracts/FamilyTree.sol
@@ -9,6 +9,7 @@ contract FamilyTree {
 		uint nodeId;
 		String name;
 		String gender;
+		unit spouseId;
 		String dateOfBirth;
 		String dateOfDeath;
 		String motherId;
@@ -26,18 +27,32 @@ contract FamilyTree {
 		node.nodeId = 0;
 	}
 
-	function addFamilyMember(uint id, String name, String gender,
-		String dateOfBirth,
-		String dateOfDeath,
-		String motherId,
-		uint fatherId)returns (uint id){
+	function addFamilyMember(uint id, String name, String gender, String dateOfBirth,String dateOfDeath)returns (uint id){
 			var node = nodes[id]
 			node.name = name;
 			node.gender = gender;
 			node.dateOfBirth = dateOfBirth;
 			node.dateOfDeath = dateOfDeath;
-			node.motherId = motherId;
-			node.fatherId = fatherId;
 			return ++id;
+	}
+
+	function addChild(uint parentId, String name, String gender, String dateOfBirth,String dateOfDeath){
+		var id = addFamilyMember(name, gender, dateOfBirth, dateOfDeath);
+		nodes[parentId].childrenIds.push(id);
+
+	}
+	function addSpouse(uint spouseId, String name, String gender, String dateOfBirth,String dateOfDeath){
+		var id = addFamilyMember(name, gender, dateOfBirth, dateOfDeath);
+		nodes[parentId].spouseId = id;
+
+	}
+	function addFather(uint childId, String name, String gender, String dateOfBirth,String dateOfDeath){
+		var id = addFamilyMember(name, gender, dateOfBirth, dateOfDeath);
+		nodes[parentId].fatherId = id;
+
+	}
+	function addMother(uint childId, String name, String gender, String dateOfBirth,String dateOfDeath){
+		var id = addFamilyMember(name, gender, dateOfBirth, dateOfDeath);
+		nodes[parentId].motherId = id;
 	}
 }
