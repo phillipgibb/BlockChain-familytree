18632f02e56f2e71e189d46cdc8592f82b59a0f0
phillipgibb <phillipgibb@gmail.com>
2017-11-21 05:56:03 +0200

    Add app stuff

---
+++ app/javascripts/contract.js
@@ -0,0 +1,97 @@
+import { default as Web3} from 'web3';
+import { default as CryptoJS } from 'crypto-js';
+
+var familyTreeABI = JSON.parse(process.env.FAMILYTREE_ABI);
+var familyTreeCode = process.env.FAMILYTREE_CODE;
+//chekout:
+//https://github.com/jsanguinetti/foodsafe/blob/e3f9b8b2d2af3e96ce46e79a3eb95748ae84d5a6/app/javascripts/app.js
+export default class ContractHelper {
+  constructor (web3) {
+    this.web3 = web3
+  }
+  
+  estimateGas (params) {
+    const estimateGasAsync = (resolve, reject) => {
+      this.web3.eth.estimateGas(
+        params,
+        (error, estimatedGas) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(estimatedGas)
+          }
+        }
+      )
+    }
+    return new Promise(estimateGasAsync)
+  }
+
+  // Not Needed?
+  // Something to have upfront so we don't compile on client machines
+  compileContract (){
+    const code = fs.readFileSync('./contracts/FamilyTree.sol').toString()
+    const compiledCode = solc.compile(code)
+    const errors = []
+    const warnings = []
+    (compiledCode.errors || []).forEach((err) => {
+      if (/:\s*Warning:/.test(err)) {
+        warnings.push(err)
+      } else {
+        errors.push(err)
+      }
+    })
+
+    if (errors.length) {
+      throw new Error('solc.compile: ' + errors.join('\n'))
+    }
+    if (warnings.length) {
+      console.warn('solc.compile: ' + warnings.join('\n'))
+    }
+    
+    familyTreeCode = compiledCode.contracts[':FamilyTree'].bytecode
+    // console.log('byteCode', byteCode);
+    
+    familyTreeABI = JSON.parse(compiledCode.contracts[':FamilyTree'].interface)
+  }
+
+  createNewContract (name, gender, dateOfBirth) {
+    web3.eth.getAccounts().then((accounts) => {
+      const estimatedGas = await estimateGas ( { data: familyTreeCode })
+      const gasPrice = web3.eth.gasPrice
+
+      console.log('gas Price: ' + gasPrice)
+      console.log('Estimated Transaction gas: ' + gasEstimate)
+      //TODO: popup to ask for password
+      console.log('unlocking Coinbase account')
+      const password = 'test1234'
+      try {
+          web3.personal.unlockAccount(web3.eth.coinbase, password)
+      } catch (e) {
+        console.log(e)
+        return
+      }
+
+      const FamilyTreeContract = new web3.eth.Contract(familyTreeABI, {data: familyTreeCode, from: accounts[0], gas: 4700000})
+      // console.log('FamilyTreeContract', FamilyTreeContract);
+
+      let deployedContract = null
+
+      FamilyTreeContract.deploy({ arguments: [name, gender, dateOfBirth]}).send(function (error, transactionHash) {
+        console.log('transactionHash', transactionHash)
+      })
+    })
+  }
+
+  findContract(contractAddress) {
+    const findAsync = (resolve, reject) => {
+      this.contract.at(contractAddress, (error, deployedContract) => {
+        if (error)
+          reject(error);
+        else
+          resolve(new ContractWrapper(deployedContract));
+      });
+    };
+    return new Promise(findAsync);
+  }
+
+}
---
+++ app/javascripts/tree.js
@@ -0,0 +1,36 @@
+const Web3 = require('web3')
+const solc = require('solc')
+const fs = require('fs')
+// const http = require('http')
+
+var options = [
+  ['Option 1', 'Option 2'],
+  ['First Option', 'Second Option', 'Third Option']
+]
+
+// get contract address if it already exists or create a new one
+
+// assuming we have done that, create tree
+
+function makeTree (array) {
+  // Create the list element:
+  var list = document.createElement('ul')
+
+  for (var i = 0; i < array.length; i++) {
+    // Create the list item:
+    var item = document.createElement('li')
+
+    // Set its contents:
+    item.appendChild(document.createTextNode(array[i]))
+
+    // Add it to the list:
+    list.appendChild(item)
+  }
+
+  // Finally, return the constructed list:
+  return list
+}
+
+// Add the contents of options[0] to #foo:
+document.getElementById('FamilyTree').appendChild(makeTree(options[0]))
+
